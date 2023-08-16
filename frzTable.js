e1e2b475418e20926f02886fff0dc2efa09276c1
Your Name <cherylrueichun@gmail.com>
2023-08-16 17:57:55 +0800

    feat: add whenClick function

--- frzTable.js
+++ frzTable.js
@@ -1,5 +1,30 @@
-$(document).ready(function () {
-  $("p").click(function () {
-    $(this).hide();
-  });
-});
+(function ($) {
+  function whenClick() {
+    console.log("click");
+  }
+
+  $.fn.frzTable = function (options) {
+    const settings = $.extend(
+      {
+        count: { slide: 1, show: 4 },
+        speed: 0.3,
+        whenClick: whenClick,
+      },
+      // frzTable fn 帶入的 options 傳入的規格，會取代上面的 settings
+      options
+    );
+
+    return this.each(function () {
+      // 將目前處理的 .frzTable 元素（即插件套用的元素）轉換成 jQuery 物件
+      const $table = $(this);
+      const count = settings.count;
+      const speed = settings.speed;
+      const whenClick = settings.whenClick;
+
+      // 其他插件的邏輯
+      $table.find(".item").click(function () {
+        whenClick($(this)); // 呼叫 whenClick 函數
+      });
+    });
+  };
+})(jQuery);
--- usage.js
+++ usage.js
@@ -1,23 +1,35 @@
-$('.frzTable.default').frzTable({
-    count: {
-        // M版時每次點擊往前往後移動幾格儲存格
-        slide: 1, // [number] 
-        // M版時一個畫面show幾格儲存格
-        show: 4 // [number] 
-    },
-    // 設定花多久時間移動完成
-    speed: .3, // [number] 
-    // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
-    whenClick: function($element) {
-        // console.log($element)
+$(".frzTable.default").frzTable({
+  count: {
+    // M版時每次點擊往前往後移動幾格儲存格
+    slide: 1, // [number]
+    // M版時一個畫面show幾格儲存格
+    show: 4, // [number]
+  },
+  // 設定花多久時間移動完成
+  speed: 0.3, // [number]
+  // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
+  whenClick: function ($element) {
+    $(".side").removeClass("side");
+    if ($element.hasClass("choose")) {
+      $element.removeClass("choose");
+      return;
+    } else {
+      $(".choose").removeClass("choose");
+      $element.addClass("choose");
+      $element.parent().addClass("side");
+      const clickedItem = $element.attr("class").split(" ")[1];
+      $(".item." + clickedItem)
+        .not(".choose")
+        .addClass("side");
     }
-})
-$('.frzTable.rel').frzTable({
-    count: {
-        slide: 1,
-        show: 2
-    },
-    whenClick: function($element) {
-        // console.log($element)
-    }
-})
+  },
+});
+$(".frzTable.rel").frzTable({
+  count: {
+    slide: 1,
+    show: 2,
+  },
+  whenClick: function ($element) {
+    // console.log($element)
+  },
+});
