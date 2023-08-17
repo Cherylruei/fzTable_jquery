d648431fb2a91a48e5fab8f6c99956cb5f5625b8
Your Name <cherylrueichun@gmail.com>
2023-08-17 17:55:15 +0800

    feat: temp2

--- frzTable.js
+++ frzTable.js
@@ -1,4 +1,17 @@
 (function ($) {
+  function adjustColumns() {
+    const screenWidth = $(window).width();
+    const $table = $(".frzTable");
+    // 抓取 7 個 column 的節點，除了需要出現的4個 column 其他隱藏
+    const columnsGroup = document.getElementsByClassName("column");
+
+    if (screenWidth <= 425) {
+      $(columnsGroup).slice(0, 4).show();
+      $(columnsGroup).slice(4, 8).hide();
+    } else {
+      $(columnsGroup).show();
+    }
+  }
   function whenClick() {
     console.log("click");
   }
@@ -6,13 +19,16 @@
   $.fn.frzTable = function (options) {
     const settings = $.extend(
       {
-        count: { slide: 1, show: 4 },
+        count: adjustColumns(1, 4),
         speed: 0.3,
         whenClick: whenClick,
       },
       // frzTable fn 帶入的 options 傳入的規格，會取代上面的 settings
       options
     );
+
+    adjustColumns();
+    $(window).on("resize", adjustColumns);
 
     return this.each(function () {
       // 將目前處理的 .frzTable 元素（即插件套用的元素）轉換成 jQuery 物件
